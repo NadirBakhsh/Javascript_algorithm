e9350f9fe15513dcbcde136058c0dadc3898136c
NadirBakhsh <nadir4k2010@gmail.com>
2022-12-23 23:59:28 +0500

    Day 6 Complete: areEquallyStrong

---
+++ areEquallyStrong/areEquallyStrong.js
@@ -0,0 +1,8 @@
+function areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) {
+	if (yourLeft === friendsLeft && yourRight === friendsRight) return true
+	return false
+}
+
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
